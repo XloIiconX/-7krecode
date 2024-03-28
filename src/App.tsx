9669bb6202fa4d28ca250dfd4747269e0a6a7964
9hodev <siaskkh4884@gmail.com>
2024-03-28 09:39:38 +0700

    update status

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+
+interface Statuses {
+  [key: string]: string;
+}
 
 function App() {
   const [token, setToken] = useState("");
+  const [statuses, setStatuses] = useState<Statuses>({}); 
 
   const api = "https://corsproxy.io/?https://coupon.netmarble.com/api/coupon";
   const couponCodes = ["FEELINGEXP", "ROCKNLUNA", "EXCELLENTEQUIPMENT", "LEGENDHUNTER", "TWINKLE7K", "STEPPETSTEP", "HEROES4U", "IAMURHERO", "UCANFINDIT", "HEREIAM", "SIRMYTHICSIR", "UPGRADECOMPLETE", "ENFORCELEGEND", "HAPPYMARCHWITHSK2", "EXPHUNTER", "DOUWANTSTONE", "POTENTIALUP", "7KLOVEYOU", "MOREMAPS", "READY2PLAYRAID", "GETAHIGHGRADE", "SHINYJEWEL", "YELLOWSOULSTONE", "EVERY1LIKESMYTH", "S2VANESSAS2"];
 
-  async function fetchCoupon(code: any) {
+  async function fetchCoupon(code: string) {
+    setStatuses(prevStatuses => ({
+      ...prevStatuses,
+      [code]: "Using" 
+    }));
+
     const payload = {
       couponCode: code,
       gameCode: "sk2gb",
@@ -33,12 +43,29 @@ function App() {
 
       const data = await response.json();
       console.log(`Coupon code ${code} response:`, data);
+
+      if(data.httpStatus==400){
+        setStatuses(prevStatuses => ({
+          ...prevStatuses,
+          [code]: "Already Use  or Fail ❌"
+        }));
+      }else{
+        setStatuses(prevStatuses => ({
+          ...prevStatuses,
+          [code]: "Get rewards ✅"
+        }));
+      }
     } catch (error) {
       console.error(`Error fetching coupon code ${code}:`, error);
+
+      setStatuses(prevStatuses => ({
+        ...prevStatuses,
+        [code]: "error"
+      }));
     }
   }
 
-  const handleTokenChange = (event: any) => {
+  const handleTokenChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setToken(event.target.value);
   }
 
@@ -66,6 +93,13 @@ function App() {
         <p>({couponCodes.length} Codes)</p>
         <input placeholder='Enter Member code' value={token} onChange={handleTokenChange}></input>
         <h2 className='btn' onClick={handleGetItemsClick}>Get Items</h2>
+        <div className="statuses">
+        {couponCodes.map(code => (
+          <h3 key={code}>
+            {code}: {statuses[code] || "unuse"} {}
+          </h3>
+        ))}
+      </div>
       </div>
       <Analytics />
     </>
